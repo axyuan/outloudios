d671a68075d612c107f122787875c05b2de399ef
Ann Yuan <ayuan@thisisgrow.com>
2015-04-03 08:22:46 -0400

    Initial Commit

---
+++ outloud/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  outloud
+//
+//  Created by Ann Yuan on 4/3/15.
+//  Copyright (c) 2015 Ann Yuan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
