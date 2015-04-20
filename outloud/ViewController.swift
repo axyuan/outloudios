17ce391fa565bb418d766402c590a2fc87a1b0cb
Ann Yuan <ayuan@thisisgrow.com>
2015-04-19 20:17:33 -0400

    some buttons

--- outloud/ViewController.swift
+++ outloud/ViewController.swift
@@ -10,8 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var recordButton: UIButton!
+    @IBOutlet weak var previewButton: UIButton!
+    @IBOutlet weak var deleteButton: UIButton!
+    
+    @IBOutlet weak var publishButton: UIButton!
+    
+    var recording: Bool = false
+    var audioFile: Bool = false
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        setRecordButtonText()
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -20,6 +30,33 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func recordAudio(sender: UIButton) {
+        recording = !recording
+        
+        if(recording == false) {
+            audioFile = true
+            recordButton.hidden = true
+            previewButton.hidden = false
+            deleteButton.hidden = false
+        }
+        
+        setRecordButtonText()
+    }
+    
+    @IBAction func deleteAudio(sender: UIButton) {
+        audioFile = false
+        recordButton.hidden = false
+        previewButton.hidden = true
+        deleteButton.hidden = true
+    }
+    
+    func setRecordButtonText() {
+        if(recording == true) {
+            recordButton.setTitle("Stop", forState: .Normal)
+        } else {
+            recordButton.setTitle("Record", forState: .Normal)
+        }
+    }
 
 }
 
