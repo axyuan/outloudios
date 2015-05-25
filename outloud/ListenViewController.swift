2c244b6145aa905b5ea886ff9cee1494080ae15d
Ann Yuan <ayuan@thisisgrow.com>
2015-05-25 13:20:44 -0400

    added modal view

---
+++ outloud/ListenViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ListenViewController.swift
+//  outloud
+//
+//  Created by Ann Yuan on 5/23/15.
+//  Copyright (c) 2015 Ann Yuan. All rights reserved.
+//
+
+import UIKit
+
+class ListenViewController: UIViewController {
+
+    @IBOutlet weak var back: UIButton!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func done(sender: UIButton) {
+        println("done")
+        self.dismissViewControllerAnimated(false, completion: nil)
+    }
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
