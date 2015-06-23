c0e0f89cbb6457ffa218a832056541cf2505f332
Ann Yuan <ayuan@thisisgrow.com>
2015-06-22 21:06:47 -0400

    add ability to delete stuff

--- outloud/ListenTableViewController.swift
+++ outloud/ListenTableViewController.swift
@@ -18,8 +18,6 @@ class ListenTableViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        println("DID LOAD TABLE VIEW")
-        println(savedFiles)
 
         // Uncomment the following line to preserve selection between presentations
         // self.clearsSelectionOnViewWillAppear = false
@@ -71,25 +69,25 @@ class ListenTableViewController: UITableViewController {
     }
     
 
-    /*
+    
     // Override to support conditional editing of the table view.
     override func tableView(tableView: UITableView, canEditRowAtIndexPath indexPath: NSIndexPath) -> Bool {
         // Return NO if you do not want the specified item to be editable.
         return true
     }
-    */
+    
 
-    /*
+    
     // Override to support editing the table view.
     override func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath) {
         if editingStyle == .Delete {
             // Delete the row from the data source
+            modalParent!.savedFiles.removeAtIndex(indexPath.row)
+            self.savedFiles.removeAtIndex(indexPath.row)
             tableView.deleteRowsAtIndexPaths([indexPath], withRowAnimation: .Fade)
-        } else if editingStyle == .Insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
+        }
     }
-    */
+    
 
     /*
     // Override to support rearranging the table view.
